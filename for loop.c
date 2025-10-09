2efc05bd33fe03c56233a1a0115e64eb22af05ca
Juanitanjoki <juanitakaruri@gmail.com>
2025-10-09 23:36:31 +0300

    Add files via upload

---
+++ for loop.c
@@ -0,0 +1,26 @@
+/*
+name:Juanita Njoki Karuri
+Reg no:CT101/G/26501/25
+description:
+Date:7/10/2025
+
+*/
+
+#include <stdio.h>
+int main() {
+	int i;
+	for (i=100; i>=50; i--)
+	
+	{
+		
+	printf("%d  \n" ,i);
+	}
+		
+		
+		return 0;
+		
+	}
+	
+		
+	
+ 
